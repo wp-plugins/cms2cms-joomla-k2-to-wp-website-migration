904da3a2fd9cc5b5f770c813272181d6b42cc143
cms2cms <cms2cms@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-14 12:57:44 +0000

    Update the Connection Bridge
    git-svn-id: https://plugins.svn.wordpress.org/cms2cms-joomla-k2-to-wp-website-migration/tags/3.6.2@913887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cms2cms-joomla-k2-to-wp-migration.php
+++ cms2cms-joomla-k2-to-wp-migration.php
@@ -1,8 +1,8 @@
 <?php
 /*
-    Plugin Name: CMS2CMS JOOMLA K2 to WordPress migration
+    Plugin Name: CMS2CMS K2 to WordPress migration
     Plugin URI: http://www.cms2cms.com
-    Description: Migrate your website content from JOOMLA K2 to WordPress easily and automatedly in just a few simple steps.
+    Description: Migrate your website content from K2 to WordPress easily and automatedly in just a few simple steps.
     Version: 1.0.1
     Author: MagneticOne
     Author URI: http://magneticone.com
